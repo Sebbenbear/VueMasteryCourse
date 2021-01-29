b340570826007da46f5223aaa2ed91456886e7db
Pauline <paulikel@amazon.com>
2021-01-29 15:34:38 +1100

    initial commit

---
+++ into-to-vue-2/1-vueinstance/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app', //element property, attach to element with id "app"
+    data: {
+        product: "Socks",
+        description: "These are beautiful socks."
+    }
+})
+// var product = "Socks";
---
+++ into-to-vue-2/3-conditional-rendering/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app', //element property, attach to element with id "app"
+    data: {
+        product: "Socks",
+        // image: './assets/vmSocks-green-onWhite.jpg',
+        image: './assets/vmSocks-blue-onWhite.png',
+        altText: 'a pair of socks',
+        imageLink: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
+        description: "These are beautiful socks."
+    }
+})
+// var product = "Socks";
