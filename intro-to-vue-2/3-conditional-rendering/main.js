ca3be44c21a2e2c8ea8a3c360fcdd9a1519a4406
Pauline <paulikel@amazon.com>
2021-01-29 15:57:23 +1100

    conditional rendering

--- intro-to-vue-2/3-conditional-rendering/main.js
+++ intro-to-vue-2/3-conditional-rendering/main.js
@@ -2,11 +2,12 @@ var app = new Vue({
     el: '#app', //element property, attach to element with id "app"
     data: {
         product: "Socks",
-        // image: './assets/vmSocks-green-onWhite.jpg',
-        image: './assets/vmSocks-blue-onWhite.png',
+        image: './assets/vmSocks-green-onWhite.jpg',
         altText: 'a pair of socks',
         imageLink: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
-        description: "These are beautiful socks."
+        description: "These are beautiful socks.",
+        inStock: true,
+        inventory: 100,
+        onSale: true
     }
 })
-// var product = "Socks";
